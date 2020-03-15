423c4ea436d144bc33bace9431af14d1ee4b8bc8
anastasia-levina <a.belov.nsk@yandex.ru>
2020-03-15 22:10:58 +0300

    5.4. Протеиновая диета.[#252229]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        while (ivan < nik) {
+            System.out.println(month);
+            month++;
+            ivan *= 3;
+            nik *= 2;
+            if (ivan > nik) {
+                break;
+            }
+        }
+        return month;
+    }
+}
