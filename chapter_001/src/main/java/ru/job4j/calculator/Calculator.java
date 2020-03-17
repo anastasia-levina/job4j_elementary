be52f7c6f71a4a6dada560085d901da9c07dbec7
anastasia-levina <a.belov.nsk@yandex.ru>
2020-03-17 22:06:39 +0300

    5.8. JavaDoc. Code convention[#252239]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,6 +1,16 @@
 package ru.job4j.calculator;
 
+/**
+ * Calculator.
+ *
+ * @author Anastasia Levina (redheadgirl.lv@gmail.com)
+ */
 public class Calculator {
+    /**
+     * Main.
+     *
+     * @param args - args.
+     */
     public static void main(String[] args) {
         int one = 1;
         int two = 2;
