eac4715229a28b34e5a0d9e3f59d42a58190a828
anastasia-levina <redheadgirl.lv@gmail.com>
2020-04-01 12:00:44 +0300

    3. Коммиты на решения задач.[#252281]

--- chapter_001/src/main/java/ru/job4j/StudentInfo.java
+++ chapter_001/src/main/java/ru/job4j/StudentInfo.java
@@ -1,6 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
+
     public static void main(String[] args) {
         System.out.println("Anastasia Levina");
         System.out.println("05.01.1996");
