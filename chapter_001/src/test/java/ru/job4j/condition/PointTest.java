3389011d05e2121971d57932aef14da33831c926
anastasia-levina <redheadgirl.lv@gmail.com>
2020-04-03 17:09:54 +0300

    4. Расстояние между точками в трехмерном пространстве.[#252330]

--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -7,47 +7,48 @@ public class PointTest {
 
     @Test
     public void result() {
-        int in = 0;
-        int in1 = 0;
-        int in2 = 2;
-        int in3 = 0;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
         double expected = 2.0;
-        double out = new Point(in, in1).distance(new Point(in2, in3));
+        double out = new Point(x1, y1).distance(new Point(x2, y2));
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void result1() {
-        int in = 2;
-        int in1 = 2;
-        int in2 = 1;
-        int in3 = 2;
+        int x1 = 2;
+        int y1 = 2;
+        int x2 = 1;
+        int y2 = 2;
         double expected = 1.0;
-        double out = new Point(in, in1).distance(new Point(in2, in3));
+        double out = new Point(x1, y1).distance(new Point(x2, y2));
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void result2() {
-        int in = 2;
-        int in1 = 2;
-        int in2 = 2;
-        int in3 = 6;
+        int x1 = 2;
+        int y1 = 2;
+        int x2 = 2;
+        int y2 = 6;
         double expected = 4.0;
-        double out = new Point(in, in1).distance(new Point(in2, in3));
+        double out = new Point(x1, y1).distance(new Point(x2, y2));
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void result3() {
-        int in = 2;
-        int in1 = 2;
-        int in2 = 2;
-        int in3 = 6;
-        int in4 = 2;
-        int in5 = 2;
+        int x1 = 2;
+        int y1 = 2;
+        int z1 = 2;
+        int x2 = 6;
+        int y2 = 2;
+        int z2 = 2;
         double expected = 4.0;
-        double out = new Point(in, in1, in2).distance3d(new Point(in3, in4, in5));
+        double out = new Point(x1, y1, z1).distance3d(new Point(x2, y2, z2));
         Assert.assertEquals(expected, out, 0.01);
     }
 }
+
